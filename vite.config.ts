3c206100626e15c5b94e1f49623634c6412c589d
Neal <winzipsdo@gmail.com>
2024-11-10 00:29:51 +0800

    feat: add pwa plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import path from 'path';
+import { VitePWA } from 'vite-plugin-pwa';
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite';
 
 // https://vite.dev/config/
@@ -18,7 +19,7 @@ export default defineConfig({
       },
     },
   },
-  plugins: [react(), TanStackRouterVite()],
+  plugins: [react(), TanStackRouterVite(), VitePWA()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
